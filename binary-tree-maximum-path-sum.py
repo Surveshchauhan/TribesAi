0cacd0f6bbd81fff9b8be012cecee334f7d98602
Survesh Chauhan <Surveshchauhan@users.noreply.github.com>
2018-11-26 18:19:38 +0530

    Update binary-tree-maximum-path-sum.py

--- binary-tree-maximum-path-sum.py
+++ binary-tree-maximum-path-sum.py
@@ -20,6 +20,5 @@ class Solution:
             return 0
         leftsum=max(0,self.findpathsum(root.left))
         rightsum=max(0,self.findpathsum(root.right))
-        root_sum = max(leftsum, rightsum,leftsum+rightsum) + root.val
-        self.globalsum= max(self.globalsum, root_sum)
+        self.globalsum= max(self.globalsum, max(leftsum, rightsum,leftsum+rightsum) + root.val)
         return  root.val + max(0,leftsum, rightsum) 
